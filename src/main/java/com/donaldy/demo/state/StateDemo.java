04a42979bd364f8e607f142c53045648417d1605
Donald <448641125@qq.com>
2021-04-23 17:36:18 +0800

    update state demo

--- src/main/java/com/donaldy/demo/state/StateDemo.java
+++ src/main/java/com/donaldy/demo/state/StateDemo.java
@@ -1,15 +1,14 @@
 package com.donaldy.demo.state;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
@@ -21,55 +20,64 @@ public class StateDemo {
 
     public static void main(String[] args) throws Exception {
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<Tuple2<Long, Long>> data = env.fromElements(Tuple2.of(1L, 3L),
-                Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 4L), Tuple2.of(1L, 2L));
-        KeyedStream<Tuple2<Long, Long>, Long> keyed = data.keyBy(value -> value.f0);
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 按照 key 分组策略，对流式数据调用状态化处理
-        SingleOutputStreamOperator<Tuple2<Long, Long>> flatMaped =
-                keyed.flatMap(new RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Long>>() {
-            private transient ValueState<Tuple2<Long, Long>> sum;
+        env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 5L), Tuple2.of(1L, 2L))
+                .keyBy(0)
+                .flatMap(new CountWindowAverage())
+                .printToErr();
 
-            // 更新 state
-            @Override
-            public void flatMap(Tuple2<Long, Long> value,
-                                Collector<Tuple2<Long, Long>> out) throws Exception {
-                // 获取当前状态值
-                Tuple2<Long, Long> currentSum = sum.value();
-                // 更新
-                currentSum.f0 += 1;
-                currentSum.f1 += value.f1;
+        env.execute("submit job");
+    }
 
-                System.out.println("...currentSum: " + currentSum);
+    public static class CountWindowAverage extends RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Long>> {
 
-                // 更新状态值
-                sum.update(currentSum);
+        private transient ValueState<Tuple2<Long, Long>> sum;
+        public void flatMap(Tuple2<Long, Long> input, Collector<Tuple2<Long, Long>> out) throws Exception {
 
-                // 如果count >= 2 清空状态值, 重新计算
-                if (currentSum.f0 >= 5) {
-                    out.collect(new Tuple2<>(value.f0, currentSum.f1 / currentSum.f0));
-                    sum.clear();
-                }
+            Tuple2<Long, Long> currentSum;
+
+            // 访问ValueState
+            if(sum.value() == null) {
+                currentSum = Tuple2.of(0L, 0L);
+            }else {
+                currentSum = sum.value();
             }
 
-            // 做出 state
-            @Override
-            public void open(Configuration parameters) {
-                ValueStateDescriptor<Tuple2<Long, Long>> descriptor = new ValueStateDescriptor<>(
-                        "average",
-                        TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {}),
-                        Tuple2.of(0L, 0L)
-                );
+            // 更新
+            currentSum.f0 += 1;
 
-                ValueStateDescriptor<Tuple2<Long, Long>> descriptor1 = new
-                        ValueStateDescriptor<>("average", TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {
-                }));
+            // 第二个元素加1
+            currentSum.f1 += input.f1;
 
-                sum = getRuntimeContext().getState(descriptor);
+            // 更新state
+            sum.update(currentSum);
+
+            // 如果count的值大于等于2，求知道并清空state
+            if (currentSum.f0 >= 2) {
+                out.collect(new Tuple2<>(input.f0, currentSum.f1 / currentSum.f0));
+                sum.clear();
             }
-        });
-        flatMaped.print();
-        env.execute();
+        }
+
+        public void open(Configuration config) {
+            ValueStateDescriptor<Tuple2<Long, Long>> descriptor =
+                    new ValueStateDescriptor<>(
+                            "average", // state的名字
+                            TypeInformation.of(new TypeHint<Tuple2<Long, Long>>() {})
+                    ); // 设置默认值
+
+
+            // 可以通过传递配置在任何状态描述符中启用 TTL 功能
+            StateTtlConfig ttlConfig = StateTtlConfig
+                    .newBuilder(Time.seconds(10))
+                    .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+                    .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+                    .build();
+
+            descriptor.enableTimeToLive(ttlConfig);
+
+            sum = getRuntimeContext().getState(descriptor);
+        }
     }
 }
