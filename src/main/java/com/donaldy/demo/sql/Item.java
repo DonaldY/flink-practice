9de63c7ba52e11279fdcd51cb3fe4963b77e1a57
Donald <448641125@qq.com>
2021-04-24 16:06:09 +0800

    add sql demo

---
+++ src/main/java/com/donaldy/demo/sql/Item.java
@@ -0,0 +1,26 @@
+package com.donaldy.demo.sql;
+
+/**
+ * @author donald
+ * @date 2021/04/24
+ */
+public class Item {
+    private String name;
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/donaldy/demo/sql/StreamingDemo.java
@@ -0,0 +1,75 @@
+package com.donaldy.demo.sql;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author donald
+ * @date 2021/04/24
+ */
+class StreamingDemo {
+    public static void main(String[] args) throws Exception {
+
+        EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment bsTableEnv = StreamTableEnvironment.create(bsEnv, bsSettings);
+
+        SingleOutputStreamOperator<Item> source = bsEnv.addSource(new MyStreamingSource()).map(new MapFunction<Item, Item>() {
+            @Override
+            public Item map(Item item) throws Exception {
+                return item;
+            }
+        });
+
+        DataStream<Item> evenSelect = source.split(new OutputSelector<Item>() {
+            @Override
+            public Iterable<String> select(Item value) {
+                List<String> output = new ArrayList<>();
+                if (value.getId() % 2 == 0) {
+                    output.add("even");
+                } else {
+                    output.add("odd");
+                }
+                return output;
+            }
+        }).select("even");
+
+        DataStream<Item> oddSelect = source.split(new OutputSelector<Item>() {
+            @Override
+            public Iterable<String> select(Item value) {
+                List<String> output = new ArrayList<>();
+                if (value.getId() % 2 == 0) {
+                    output.add("even");
+                } else {
+                    output.add("odd");
+                }
+                return output;
+            }
+        }).select("odd");
+
+
+        bsTableEnv.createTemporaryView("evenTable", evenSelect, "name,id");
+        bsTableEnv.createTemporaryView("oddTable", oddSelect, "name,id");
+
+        Table queryTable = bsTableEnv.sqlQuery("select a.id,a.name,b.id,b.name from evenTable as a join oddTable as b on a.name = b.name");
+
+        queryTable.printSchema();
+
+        bsTableEnv.toRetractStream(queryTable, TypeInformation.of(new TypeHint<Tuple4<Integer,String,Integer,String>>(){})).print();
+
+        bsEnv.execute("streaming sql job");
+    }
+
+}
